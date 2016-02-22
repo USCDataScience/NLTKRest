8964018fca472317de237759555bf1cd1edbaeed
Chris Mattmann <mattmann@apache.org>
2016-02-21 19:11:20 -0800

    Finish off server and pretty print JSON.

--- nltkrest/nltkrest/server.py
+++ nltkrest/nltkrest/server.py
@@ -60,29 +60,26 @@ def namedEntityRecognizer():
     timer = timex.tag(content)
     tokenized = nltk.word_tokenize(content)
     tagged = nltk.pos_tag(tokenized)
-    namedEnt = []
-    Entities = {'PERSON':set(), 'LOCATION':set(), 'GPE':set(), 'FACILITY':set()}
-    Entities['TIME'] = timer
-    NE = set()
     namedEnt = nltk.ne_chunk(tagged, binary=True)
-    for each_ne in namedEnt:
-        if isinstance(each_ne, nltk.tree.Tree):
-            value = ''
-            for i in range(0,len(each_ne)):
-                value += each_ne[i][0] + ' '
-            value = value.strip()
-            NE.add(value)
-
-    result = {}
-    for each in Entities:
-        if len(Entities[each]) is 0:
-            continue
-        else:
-            result[each] = list(Entities[each])
-    j = json.dumps(result)
+    names = extract_entity_names(namedEnt)
+    result = {"result" : "success", "names" : names}
+    jsonDoc = json.dumps(result, sort_keys=True, indent=4, separators=(',', ': '))
     end = time.time()
-    print(end - start)
-    return j
+    print "NER took "+str(end - start)+" seconds"
+    return jsonDoc
+
+
+# Based on example from:
+# https://gist.github.com/onyxfish/322906
+def extract_entity_names(t):
+    entity_names = []
+    if hasattr(t, 'label') and t.label:
+        if t.label() == 'NE':
+            entity_names.append(' '.join([child[0] for child in t]))
+        else:
+            for child in t:
+                entity_names.extend(extract_entity_names(child))
+    return entity_names
 
 def main(argv=None):
     """Run NLTK REST server from command line according to USAGE."""

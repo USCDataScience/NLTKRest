924839d61f6f92d19c8bff8cf53efa8f33728831
manali <manalishah.91@gmail.com>
2016-03-11 19:39:53 -0500

    updated viz code

--- nltkrest-examples/src/main/java/edu/usc/ir/visualization/NLTKandCoreNLP.java
+++ nltkrest-examples/src/main/java/edu/usc/ir/visualization/NLTKandCoreNLP.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package src.main.java.edu.usc.ir.visualization;
+package edu.usc.ir.visualization;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
